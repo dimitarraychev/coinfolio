83a09a0fa6c99fc07b71e1217528616f3bfee8a0
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-03 19:33:27 +0300

    Created home portfolio section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import heroLeft from "../../assets/images/hero-left.svg";
 import heroRight from "../../assets/images/hero-right.svg";
+import homePortfolio from "../../assets/images/home-portfolio.svg";
+import homeExplore from "../../assets/images/home-explore.svg";
 
 import { CoinContext } from "../../context/CoinContext";
 import { CoinTableRow } from "../../components/CoinTableRow/CoinTableRow";
@@ -55,6 +57,44 @@ const Home = () => {
 				<Link to={"/rankings"} className="home-btn-rankings">
 					<Button text="Check Rankings" />
 				</Link>
+			</section>
+
+			<section className="home-portfolio">
+				<div className="home-portfolio-text">
+					<p className="subheading">global rankings</p>
+					<h2>Build Your Portfolio & Monitor Performance</h2>
+					<p>
+						Easily create your personalized crypto portfolio and
+						stay updated on its performance. Our intuitive platform
+						allows you to track price changes, monitor market
+						trends, and manage your investments all in one place.
+					</p>
+
+					<Link to={"/hub"} className="home-btn-portfolio">
+						<Button text="Portfolio Hub" />
+					</Link>
+				</div>
+
+				<img src={homePortfolio} alt="portfolio" />
+			</section>
+
+			<section className="home-explore">
+				<img src={homeExplore} alt="portfolio" />
+
+				<div className="home-explore-text">
+					<p className="subheading">monitor markets</p>
+					<h2>Discover Crypto Sectors & Market Trends</h2>
+					<p>
+						Easily create your personalized crypto portfolio and
+						stay updated on its performance. Our intuitive platform
+						allows you to track price changes, monitor market
+						trends, and manage your investments all in one place.
+					</p>
+
+					<Link to={"/explore"} className="home-btn-explore">
+						<Button text="Explore" />
+					</Link>
+				</div>
 			</section>
 		</div>
 	);
