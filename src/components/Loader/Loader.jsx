67225644e1687c4c09747570c0e9a805958a4ae7
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-03 13:38:20 +0300

    Created Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = (props) => {
+	return (
+		<div className="dot-spinner" style={{ "--uib-size": props.size }}>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+			<div className="dot-spinner__dot"></div>
+		</div>
+	);
+};
+
+export default Loader;
