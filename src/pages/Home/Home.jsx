e7ee82b8c6b4c309f20312c02be562509f0b1399
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-03 17:51:47 +0300

    Added rankings section to Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
 import "./Home.css";
 import heroLeft from "../../assets/images/hero-left.svg";
 import heroRight from "../../assets/images/hero-right.svg";
+
+import { CoinContext } from "../../context/CoinContext";
+import { CoinTableRow } from "../../components/CoinTableRow/CoinTableRow";
+import CryptoTable from "../../components/CryptoTable/CryptoTable";
 import Button from "../../components/Button/Button";
 
 const Home = () => {
+	const { allCoins } = useContext(CoinContext);
+
 	return (
 		<div className="home">
 			<section className="hero">
 				<img src={heroLeft} alt="hero-left" />
+
 				<article className="hero-text">
 					<p className="subheading">real-time tracking</p>
 					<h1>Your #1 Trusted Source for Everything Crypto</h1>
+
 					<p className="description">
 						Stay ahead of the curve with our comprehensive platform.
 						Get real-time price updates, in-depth market data, and
 						all the latest information on a wide variety of
 						cryptocurrencies.
 					</p>
+
 					<div className="btn-container">
-						<Button text="Get Started" />
-						<Button text="Explore" isGhost={true} />
+						<Link to={"/"}>
+							<Button text="Get Started" />
+						</Link>
+						<Link to={"/explore"}>
+							<Button text="Explore" isGhost={true} />
+						</Link>
 					</div>
 				</article>
+
 				<img src={heroRight} alt="hero-right" />
+			</section>
+
+			<section className="home-rankings">
+				<p className="subheading">global rankings</p>
+				<h2>Top Cryptocurrencies By Market Cap</h2>
+
+				<CryptoTable>
+					{allCoins.slice(0, 3).map((item, index) => (
+						<CoinTableRow item={item} key={index} />
+					))}
+				</CryptoTable>
+
+				<Link to={"/rankings"} className="home-btn-rankings">
+					<Button text="Check Rankings" />
+				</Link>
 			</section>
 		</div>
 	);
