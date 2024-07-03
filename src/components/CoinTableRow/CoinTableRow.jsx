e6aa3b38da9deb3cdbce7692c3c2299368af397b
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-03 17:50:09 +0300

    Renamed CoinRanking to CoinTableRow

---
+++ src/components/CoinTableRow/CoinTableRow.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import "./CoinTableRow.css";
+import arrowUp from "../../assets/icons/arrow-up.svg";
+import arrowDown from "../../assets/icons/arrow-down.svg";
+
+import { Link } from "react-router-dom";
+import { CoinContext } from "../../context/CoinContext";
+import formatPrice from "../../utils/format-price";
+
+export const CoinTableRow = ({ item }) => {
+	const { allCoins, currency } = useContext(CoinContext);
+	const isPositivePriceChange = item.price_change_percentage_24h > 0;
+
+	return (
+		<Link to={`/coin/${item.id}`} className="table-layout">
+			<p>{item.market_cap_rank}</p>
+			<div>
+				<img src={item.image} alt={item.symbol} />
+				<p>{item.name + " - " + item.symbol.toUpperCase()}</p>
+			</div>
+			<p>
+				{currency.symbol}
+				{formatPrice(item.current_price)}
+			</p>
+			<p className={isPositivePriceChange ? "green" : "red"}>
+				{Math.floor(item.price_change_percentage_24h * 100) / 100}%
+				<img
+					className="arrow"
+					src={isPositivePriceChange ? arrowUp : arrowDown}
+					alt="arrow"
+				/>
+			</p>
+			<p className="market-cap">
+				{currency.symbol}
+				{item.market_cap.toLocaleString()}
+			</p>
+		</Link>
+	);
+};
