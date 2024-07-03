67c48f0b63aff10a34aa568bed4e9fa1b818b640
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-03 10:48:50 +0300

    Initiated some view on the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./Home.css";
+import heroLeft from "../../assets/images/hero-left.svg";
+import heroRight from "../../assets/images/hero-right.svg";
 
 const Home = () => {
 	return (
 		<div className="home">
-			<div className="hero">
-				<h1>
-					Largest <br /> Crypto Marketplace
-				</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Et, inventore reiciendis iusto blanditiis illo nobis.
-					Cupiditate sed in ea totam voluptatum iure iusto accusantium
-					nisi, ducimus mollitia tempora, amet ratione!
-				</p>
-			</div>
+			<section className="hero">
+				<img src={heroLeft} alt="hero-left" />
+				<article className="hero-text">
+					<p className="subheading">real-time tracking</p>
+					<h1>Your #1 Trusted Source for Everything Crypto</h1>
+					<p className="description">
+						Stay ahead of the curve with our comprehensive platform.
+						Get real-time price updates, in-depth market data, and
+						all the latest information on a wide variety of
+						cryptocurrencies.
+					</p>
+					<div className="btn-container"></div>
+				</article>
+				<img src={heroRight} alt="hero-right" />
+			</section>
 		</div>
 	);
 };
