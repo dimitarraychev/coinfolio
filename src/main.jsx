47650a95f973c2e41c138dccc1f39261330ffb1e
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-05 10:58:40 +0300

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import CoinContextProvider from "./context/CoinContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<CoinContextProvider>
-				<App />
-			</CoinContextProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<CoinContextProvider>
+			<App />
+		</CoinContextProvider>
+	</BrowserRouter>
 );
