7f33c3eb1c507732d862a47d105fe85e00d2e273
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-05 15:04:13 +0300

    Button receives isWide prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,8 +3,17 @@ import "./Button.css";
 
 const Button = (props) => {
 	return (
-		<div className={props.isGhost ? "btn btn-ghost" : "btn"}>
+		<div
+			className={
+				props.isGhost
+					? "btn btn-ghost"
+					: props.isWide
+					? "btn btn-wide"
+					: "btn"
+			}
+		>
 			{props.text}
+			{props.svg && <img src={props.svg} />}
 		</div>
 	);
 };
