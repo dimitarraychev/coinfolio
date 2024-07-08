fa9f914dd3e8f59d787ec68d16802e9cdda404cd
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-08 19:00:55 +0300

    Added red bg for delete btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,11 +4,18 @@ import { buttonIcons } from "../../constants/button";
 
 const Button = ({ text, isGhost, isWide }) => {
 	const hasSVG = buttonIcons.find((btn) => btn.text.includes(text));
+	const isDelete = text === "delete";
 
 	return (
 		<button
 			className={
-				isGhost ? "btn btn-ghost" : isWide ? "btn btn-wide" : "btn"
+				isGhost
+					? "btn btn-ghost"
+					: isWide
+					? "btn btn-wide"
+					: isDelete
+					? "btn btn-del"
+					: "btn"
 			}
 		>
 			{text}
