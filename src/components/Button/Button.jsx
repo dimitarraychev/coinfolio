559cbc3151b72068cc933c47e909c63b44a55831
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-09 00:20:34 +0300

    Added onClick to btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Button.css";
 import { buttonIcons } from "../../constants/button";
 
-const Button = ({ text, isGhost, isWide }) => {
+const Button = ({ text, isGhost, isWide, type, onClick }) => {
 	const hasSVG = buttonIcons.find((btn) => btn.text.includes(text));
 	const isDelete = text === "delete";
 
 	return (
 		<button
+			type={type != null ? type : "button"}
+			onClick={onClick}
 			className={
 				isGhost
 					? "btn btn-ghost"
