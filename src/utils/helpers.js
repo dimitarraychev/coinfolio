f1cb35454e4b7d755fe966192c4a4fc9bb99ae34
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-08 13:21:54 +0300

    Added safet y check to util

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,8 @@ export default function formatPrice(price) {
 	const minimumFractionDigits = 2;
 	const maximumFractionDigits = 8;
 
+	if (price == null) return 0;
+
 	return price.toLocaleString(undefined, {
 		minimumFractionDigits,
 		maximumFractionDigits,
