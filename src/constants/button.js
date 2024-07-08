8cf658b4b13b82142098be9333a27e5fe56ef1e6
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-08 19:38:14 +0300

    Added more icons to button

--- src/constants/button.js
+++ src/constants/button.js
@@ -1,18 +1,43 @@
 import plusIcon from "../assets/icons/plus-icon.svg";
 import arrowRight from "../assets/icons/arrow-right.svg";
-import createIcon from "../assets/icons/create-icon.svg";
+import followIcon from "../assets/icons/follow-icon.svg";
+import deleteIcon from "../assets/icons/delete-icon.svg";
+import editIcon from "../assets/icons/edit-icon.svg";
+import exploreIcon from "../assets/icons/explore-icon-white.svg";
+import rankingsIcon from "../assets/icons/rank-icon-white.svg";
+import portfolioIcon from "../assets/icons/portfolio-icon-white.svg";
 
 export const buttonIcons = [
 	{
 		text: ["follow"],
-		svg: plusIcon,
+		svg: followIcon,
 	},
 	{
 		text: ["create portfolio"],
-		svg: createIcon,
+		svg: plusIcon,
 	},
 	{
-		text: ["sign up", "sign in"],
+		text: ["sign up", "sign in", "get started", "join our platform now"],
 		svg: arrowRight,
+	},
+	{
+		text: ["delete"],
+		svg: deleteIcon,
+	},
+	{
+		text: ["edit"],
+		svg: editIcon,
+	},
+	{
+		text: ["explore"],
+		svg: exploreIcon,
+	},
+	{
+		text: ["check rankings"],
+		svg: rankingsIcon,
+	},
+	{
+		text: ["check portfolios"],
+		svg: portfolioIcon,
 	},
 ];
