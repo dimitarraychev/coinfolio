8e2c223869782c52d36f7699498dacb565b18fca
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-08 13:22:58 +0300

    Added categories and create to Hub

--- src/pages/PortfolioHub/PortfolioHub.jsx
+++ src/pages/PortfolioHub/PortfolioHub.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PortfolioHub.css";
 import hubIcon from "../../assets/icons/portfolio-icon-white.svg";
 import PortfolioTableRow from "../../components/PortfolioTableRow";
 import CryptoTable from "../../components/CryptoTable/CryptoTable";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import CategoriesMenu from "../../components/CategoriesMenu/CategoriesMenu";
+import { portfolioCategories } from "../../constants/categories";
 
 const PortfolioHub = () => {
+	const [category, setCategory] = useState("all");
+	const [isLoading, setIsLoading] = useState(false);
+
+	const categoriesHandler = (value) => {
+		setIsLoading(true);
+		setCategory(value);
+	};
+
+	useEffect(() => {
+		console.log(category);
+	}, [category]);
+
 	return (
 		<section className="hub">
 			<h2 className="page-header">
 				<img src={hubIcon} alt="hubIcon" />
 				Portfolio Hub
 			</h2>
+
+			<section className="portfolio-cta">
+				<div className="cta-text">
+					<h3>Join The Crypto Community</h3>
+					<p>
+						Manage and compare your Portfolio's performance with the
+						community.
+					</p>
+				</div>
+				<Link to={"/hub/create"}>
+					<Button text="Create Portfolio"></Button>
+				</Link>
+			</section>
+
+			<CategoriesMenu
+				categories={portfolioCategories}
+				category={category}
+				onCategoryChange={categoriesHandler}
+			/>
 
 			<div className="portfolios-wrapper">
 				<CryptoTable
