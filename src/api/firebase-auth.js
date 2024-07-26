0da2a80219151cff1af3a1352ed9b25e8d041c79
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-26 15:56:03 +0300

    Created firbase auth api f-ns

---
+++ src/api/firebase-auth.js
@@ -0,0 +1,46 @@
+import { auth } from "../config/firebase";
+import { toast } from "react-toastify";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	updateProfile,
+	onAuthStateChanged,
+} from "firebase/auth";
+
+export const register = async (username, email, password) => {
+	try {
+		await createUserWithEmailAndPassword(auth, email, password);
+		await updateProfile(auth.currentUser, { displayName: username });
+
+		toast.success(`Success! Welcome to CoinFol.io, ${username}.`);
+	} catch (error) {
+		toast.error(`Error! ${error.code}: ${error.message}`);
+	}
+};
+
+export const login = async (email, password) => {
+	try {
+		await signInWithEmailAndPassword(auth, email, password);
+
+		toast.success(
+			`Success! CoinFol.io welcomes you back, ${auth.currentUser.displayName}.`
+		);
+	} catch (error) {
+		toast.error(`Error! ${error.code}: ${error.message}`);
+	}
+};
+
+export const logout = async () => {
+	try {
+		await signOut(auth);
+
+		toast.success(`Success! "You've logged out, come back soon.`);
+	} catch (error) {
+		toast.error(`Error! ${error.code}: ${error.message}`);
+	}
+};
+
+export const checkAuth = (callback) => {
+	onAuthStateChanged(auth, callback);
+};
