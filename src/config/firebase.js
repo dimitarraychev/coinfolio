d3d260e67be386f314f132d61a106d1bd92a5d11
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-26 15:55:45 +0300

    Installed and configured firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAn-9Ourkf8F-7BWuNkHMUQxe_VyPFYbkk",
+	authDomain: "coinfolio-109f2.firebaseapp.com",
+	projectId: "coinfolio-109f2",
+	storageBucket: "coinfolio-109f2.appspot.com",
+	messagingSenderId: "709338014181",
+	appId: "1:709338014181:web:847699798b7c2cf43c4f0c",
+	measurementId: "G-MRSBNL4WKW",
+};
+
+// const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
