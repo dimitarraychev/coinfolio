ebad1c599662fd3deab979264ba51c848bf639ce
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-30 17:29:29 +0300

    Implemented isLoading in Route Guards

--- src/components/AuthRedirect/AuthRedirect.jsx
+++ src/components/AuthRedirect/AuthRedirect.jsx
@@ -1,8 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useCurrentUser } from "../../context/AuthContext";
+import Loader from "../Loader/Loader";
 
 const AuthRedirect = () => {
-	const { isAuthenticated } = useCurrentUser();
+	const { isAuthenticated, isLoading } = useCurrentUser();
+
+	if (isLoading)
+		return (
+			<div className="loading loading-80">
+				<Loader size={"15rem"} />
+			</div>
+		);
+
 	return isAuthenticated ? <Navigate to="/profile" /> : <Outlet />;
 };
 
--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useCurrentUser } from "../../context/AuthContext";
+import Loader from "../Loader/Loader";
 
 const ProtectedRoute = () => {
-	const { isAuthenticated } = useCurrentUser();
+	const { isAuthenticated, isLoading } = useCurrentUser();
+
+	if (isLoading)
+		return (
+			<div className="loading loading-80">
+				<Loader size={"15rem"} />
+			</div>
+		);
+
 	return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
