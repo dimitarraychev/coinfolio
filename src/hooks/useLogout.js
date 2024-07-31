8b11663190a897552b04ada972a499a465ec7e6f
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-31 16:42:45 +0300

    Refactored useLogout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 import { useConfirmModalContext } from "../context/ConfirmModalContext";
 import { logout } from "../api/firebase-auth";
 
@@ -9,8 +11,18 @@ const useLogout = () => {
 	const logoutHandler = () => {
 		openConfirmModal(
 			"Are you sure you want to sign off from your account?",
-			() => logout(navigate)
+			initiateLogout
 		);
+	};
+
+	const initiateLogout = async () => {
+		try {
+			await logout();
+			toast.success(`Success! You have logged out, come back soon.`);
+			navigate("/");
+		} catch (error) {
+			toast.error(`Error! Please try again.`);
+		}
 	};
 
 	return {
