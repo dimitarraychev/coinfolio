9888fcaf4f4c804c911c2ee27faf1a75f06693f9
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-31 13:57:35 +0300

    Created and implemented useGetCoins hook

---
+++ src/hooks/useGetCoins.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { fetchAllCoins, searchAllCoins } from "../api/coinGecko";
+
+const useGetCoins = (defaultCategory, allCoins, currency) => {
+	const [coins, setCoins] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const category = searchParams.get("category");
+	const searchQuery = searchParams.get("search");
+
+	const categoriesHandler = (value) => {
+		if (searchParams.get("category") === value) return;
+		setIsLoading(true);
+		setSearchParams({ category: value });
+	};
+
+	const searchHandler = (input) => {
+		if (!input) return;
+		setIsLoading(true);
+		setSearchParams({ search: input });
+	};
+
+	const searchCoins = async (input) => {
+		const filteredCoins = allCoins.filter((coin) =>
+			coin.name.toLowerCase().includes(input.toLowerCase())
+		);
+		setCoins(filteredCoins);
+		setIsLoading(false);
+
+		// try {
+		// 	const search = await searchAllCoins(input);
+		// 	setCoins(search.coins);
+		// 	setIsLoading(false);
+		// } catch (error) {
+		// 	console.error("Failed to fetch coins:", error);
+		// }
+	};
+
+	const fetchCoins = async () => {
+		try {
+			const coins = await fetchAllCoins(currency.name, category);
+			setCoins(coins);
+		} catch (error) {
+			toast.error("Error! Failed to fetch coins: ", error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		if (searchQuery) {
+			searchCoins(searchQuery);
+			return;
+		}
+
+		if (category === "all") {
+			setCoins(allCoins);
+			setIsLoading(false);
+			return;
+		}
+
+		fetchCoins();
+	}, [category, allCoins, currency, searchParams]);
+
+	return {
+		coins,
+		category,
+		isLoading,
+		categoriesHandler,
+		searchHandler,
+	};
+};
+
+export default useGetCoins;
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,80 +1,21 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-
 import "./Explore.css";
 import exploreIcon from "../../assets/icons/explore-icon-white.svg";
 
 import { useCoinContext } from "../../context/CoinContext";
 import CryptoTable from "../../components/CryptoTable/CryptoTable";
 import CoinTableRow from "../../components/CoinTableRow/CoinTableRow";
-import { fetchAllCoins, searchAllCoins } from "../../api/coinGecko";
 import Loader from "../../components/Loader/Loader";
 import CategoriesMenu from "../../components/CategoriesMenu/CategoriesMenu";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import useGetCoins from "../../hooks/useGetCoins";
 import { exploreCategories } from "../../constants/categories";
-import { toast } from "react-toastify";
 
 const Explore = () => {
 	const { allCoins, currency } = useCoinContext();
-	const [displayCoins, setDisplayCoins] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [searchParams, setSearchParams] = useSearchParams();
 	const defaultCategory = "all";
-	const category = searchParams.get("category");
-	const searchQuery = searchParams.get("search");
 
-	const categoriesHandler = (value) => {
-		if (searchParams.get("category") === value) return;
-		setIsLoading(true);
-		setSearchParams({ category: value });
-	};
-
-	const searchHandler = (input) => {
-		if (!input) return;
-		setIsLoading(true);
-		setSearchParams({ search: input });
-	};
-
-	const searchCoins = async (input) => {
-		const filteredCoins = allCoins.filter((coin) =>
-			coin.name.toLowerCase().includes(input.toLowerCase())
-		);
-		setDisplayCoins(filteredCoins);
-		setIsLoading(false);
-
-		// try {
-		// 	const search = await searchAllCoins(input);
-		// 	setDisplayCoins(search.coins);
-		// 	setIsLoading(false);
-		// } catch (error) {
-		// 	console.error("Failed to fetch coins:", error);
-		// }
-	};
-
-	const fetchCoins = async () => {
-		try {
-			const coins = await fetchAllCoins(currency.name, category);
-			setDisplayCoins(coins);
-			setIsLoading(false);
-		} catch (error) {
-			toast.error("Error! Failed to fetch coins: ", error);
-		}
-	};
-
-	useEffect(() => {
-		if (searchQuery) {
-			searchCoins(searchQuery);
-			return;
-		}
-
-		if (category === "all") {
-			setDisplayCoins(allCoins);
-			setIsLoading(false);
-			return;
-		}
-
-		fetchCoins();
-	}, [category, allCoins, currency, searchParams]);
+	const { coins, category, isLoading, categoriesHandler, searchHandler } =
+		useGetCoins(defaultCategory, allCoins, currency);
 
 	return (
 		<section className="explore">
@@ -110,7 +51,7 @@ const Explore = () => {
 						<Loader />
 					</div>
 				) : (
-					displayCoins.map((coin) => (
+					coins.map((coin) => (
 						<CoinTableRow coin={coin} key={coin.id} />
 					))
 				)}
