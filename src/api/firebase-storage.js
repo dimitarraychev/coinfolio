687bb1b6b09c0abefd55ede75996c8ffbb391c6d
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-03 16:16:58 +0300

    Created firebase-storage in api and upload file f-n

---
+++ src/api/firebase-storage.js
@@ -0,0 +1,16 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../config/firebase";
+
+export const uploadFile = async (file) => {
+	const filePath = "users/" + Date.now() + "_" + file.name;
+	const storageRef = ref(storage, filePath);
+
+	try {
+		const snapshot = await uploadBytes(storageRef, file);
+		const fileUrl = await getDownloadURL(snapshot.ref);
+
+		return fileUrl;
+	} catch (error) {
+		throw error;
+	}
+};
