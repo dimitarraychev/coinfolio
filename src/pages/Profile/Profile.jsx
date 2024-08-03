c71a39df4117663d2a304c60016058f298516643
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-03 16:19:30 +0300

    Added profile image upload to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
+
 import "./Profile.css";
 import profileIcon from "../../assets/icons/profile-icon.svg";
+import uploadIcon from "../../assets/icons/upload-icon.svg";
 import userPlaceholder from "../../assets/user-placeholder.svg";
+import arrowScroll from "../../assets/icons/arrow-scroll.svg";
 
 import { useAuthContext } from "../../context/AuthContext";
 import PortfolioTableRow from "../../components/PortfolioTableRow/PortfolioTableRow";
@@ -9,10 +13,15 @@ import CategoriesMenu from "../../components/CategoriesMenu/CategoriesMenu";
 import Loader from "../../components/Loader/Loader";
 import useGetPorfolios from "../../hooks/useGetPortfolios";
 import { profileCategories } from "../../constants/categories";
+import FileUploader from "../../components/FileUploader/FileUploader";
+import { useState } from "react";
 
 const Profile = () => {
 	const { currentUser } = useAuthContext();
 	const defaultCategory = "owned";
+	const [isUploaderOpen, setIsUploaderOpen] = useState(false);
+
+	const canChangeProfileImage = currentUser.signInMethod === "password";
 
 	const {
 		portfolios,
@@ -37,11 +46,28 @@ const Profile = () => {
 			</h2>
 
 			<div className="profile-data">
-				<img
-					src={currentUser?.photoURL || userPlaceholder}
-					alt="profile"
-					className={"profile-img"}
-				/>
+				<div className="image-wrapper">
+					<img
+						src={currentUser?.photoURL || userPlaceholder}
+						alt="profile"
+						className={"profile-img"}
+					/>
+					{canChangeProfileImage && (
+						<>
+							<img
+								src={uploadIcon}
+								alt="edit"
+								className="edit-profile-img-icon"
+								title="Upload Image"
+								onClick={() => setIsUploaderOpen(true)}
+							/>
+							<FileUploader
+								isOpen={isUploaderOpen}
+								closeUploader={() => setIsUploaderOpen(false)}
+							/>
+						</>
+					)}
+				</div>
 				<div className="list-wrapper">
 					<ul>
 						<li>Username</li>
@@ -91,13 +117,38 @@ const Profile = () => {
 					) : noResultsMessage !== "" ? (
 						<h6 className="no-portfolios">{noResultsMessage}</h6>
 					) : (
-						portfolios.map((portfolio, index) => (
-							<PortfolioTableRow
-								portfolio={portfolio}
-								key={portfolio.id}
-								index={index + 1}
-							/>
-						))
+						<>
+							{portfolios.map((portfolio, index) => (
+								<PortfolioTableRow
+									portfolio={portfolio}
+									key={portfolio.id}
+									index={index + 1}
+								/>
+							))}
+							<div className="end-message-wrapper">
+								<p className="end-message">
+									You've reached the end. Keep exploring or{" "}
+									<Link
+										to={"/hub/create"}
+										className="create-link"
+									>
+										create a portfolio!
+									</Link>
+								</p>
+								<img
+									src={arrowScroll}
+									alt="top"
+									title="Back To Top"
+									className="scroll-top-image"
+									onClick={() =>
+										window.scrollTo({
+											top: 0,
+											behavior: "smooth",
+										})
+									}
+								/>
+							</div>
+						</>
 					)}
 				</CryptoTable>
 			</div>
